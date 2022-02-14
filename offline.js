a7538c798193774eef1f592de672b31e9ba38d0e
emrah tobbas <emrahtobbas@gmail.com>
2022-02-15 01:29:10 +0300

    v1

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1644877559,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/sayidogrusu-sheet0.png",
+		"images/a1-sheet0.png",
+		"images/kesir-sheet0.png",
+		"images/kesir-sheet1.png",
+		"images/kesir-sheet2.png",
+		"images/devamspt-sheet0.png",
+		"media/dogruses (online-audio-converter.com).ogg",
+		"media/yanlisses (online-audio-converter.com).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
